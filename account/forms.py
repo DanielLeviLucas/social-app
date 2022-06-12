471bf90c967c2cc41cccc079083ea5133524bc54
Daniel Sam <daniellevilucas@gmail.com>
2022-06-13 02:41:49 +0530

    Add UserRegistrationForm

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    repeat_password = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_repeat_password(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['password'] != cleaned_data['repeat_password']:
+            raise forms.ValidationError("Password doesn't match")
+        return cleaned_data['password']
