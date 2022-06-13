3cbb99a68cf90540cab31cfd82d4fbfd36ec362a
Daniel Sam <daniellevilucas@gmail.com>
2022-06-13 10:46:47 +0530

    Fix timestamp format

--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%y/%y/%d/',
+    photo = models.ImageField(upload_to='users/%y/%m/%d/',
                               blank=True)
 
     def __str__(self):
