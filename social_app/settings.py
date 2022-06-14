42d16f590d2198ef006489f280103d4694835e71
Daniel Sam <daniellevilucas@gmail.com>
2022-06-14 11:22:34 +0530

    Create images app & Add app to INSTALLED_APPS

--- social_app/settings.py
+++ social_app/settings.py
@@ -49,6 +49,7 @@ ALLOWED_HOSTS = ['testurl.in', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'social_django',
     'account.apps.AccountConfig',
     'django.contrib.admin',
