7347857a6a5384bae2a2b97cb9e6fbd683807eb6
Daniel Sam <daniellevilucas@gmail.com>
2022-06-15 16:25:39 +0530

    Create custom ajax_required decorator

---
+++ common/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponseBadRequest
+
+
+def ajax_required(f):
+    def wrap(request, *args, **kwargs):
+        is_ajax = request.headers.get('x-requested-with') == 'XMLHttpRequest'
+
+        if not is_ajax:
+            return HttpResponseBadRequest()
+        return f(request, *args, **kwargs)
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
+    return wrap
