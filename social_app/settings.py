b722c6cc26096aac29ead9ba9b2c49985808c9ce
Daniel Sam <daniellevilucas@gmail.com>
2022-06-15 12:03:01 +0530

    Add easy_thumbnails in installed apps & template

--- social_app/settings.py
+++ social_app/settings.py
@@ -49,6 +49,7 @@ ALLOWED_HOSTS = ['testurl.in', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'easy_thumbnails',
     'images.apps.ImagesConfig',
     'social_django',
     'account.apps.AccountConfig',
