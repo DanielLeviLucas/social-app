110aa6b02ccde03204340f7c37020bf36826936c
Daniel Sam <daniellevilucas@gmail.com>
2022-06-15 13:44:26 +0530

    Fix field name

--- images/models.py
+++ images/models.py
@@ -15,9 +15,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
-    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                       related_name='images_liked',
-                                       blank=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def __str__(self):
         return self.title
