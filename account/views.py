d8c1b0c96080619230f4bff79d656b9585ebef9e
Daniel Sam <daniellevilucas@gmail.com>
2022-06-16 12:08:51 +0530

    Add user_follow view & domready template

--- account/views.py
+++ account/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm, \
-    UserEditForm, ProfileEditForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Contact
 # Create your views here.
 
 
@@ -78,3 +79,25 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'OK'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ERROR'})
+    return JsonResponse({'status': 'ERROR'})
